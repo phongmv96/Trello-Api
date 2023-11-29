87083c05927df40d1c85acac90cbf768e7e228f4
phong.mv <phongmv@ycomm.vn>
2023-11-29 11:09:43 +0700

    update server file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,5 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- * Sample Eslint config for NodeJS ExpressJS MongoDB project
- */
 module.exports = {
-  env: { es2020: true, node: true },
+  env: {es2020: true, node: true},
   extends: [
     'eslint:recommended'
   ],
